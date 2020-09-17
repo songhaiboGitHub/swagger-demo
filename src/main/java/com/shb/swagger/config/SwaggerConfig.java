c3978a74ec34ab1b7a06df37b934cf7b3c194b26
songhaibo <594859595@qq.com>
2020-09-17 17:22:53 +0800

    Swagger启动测试

---
+++ src/main/java/com/shb/swagger/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.shb.swagger.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author
+ * @version 创建时间：2020/9/17 17:11
+ */
+@Configuration
+@EnableSwagger2   //开启Swagger
+public class SwaggerConfig {
+}
